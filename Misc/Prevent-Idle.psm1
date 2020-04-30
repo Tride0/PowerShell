ee8e237ebe1f61e1457135710b1ea0114c17295d
Kyle <34794736+Tride0@users.noreply.github.com>
2020-04-30 08:18:59 -0700

    Update and rename Prevent-Idle.ps1 to Prevent-Idle.psm1

---
+++ Misc/Prevent-Idle.psm1
@@ -0,0 +1,30 @@
+Function Prevent-Idle
+{
+    <#
+        .NOTES
+            Created By: Kyle Hewitt
+            Created In: 2019
+            Last Edit: 4/30/20
+            Version: 1.0.0
+	
+        .DESCRIPTION
+	    Send Keys Reference: https://ss64.com/vb/sendkeys.html
+    #>
+    Param(
+        $SendKey = "SCROLLLOCK",
+        $IntervalSeconds = 600
+    )
+    Begin
+    {
+        $ObjShell = New-Object -ComObject wscript.shell
+    }
+    Process
+    {
+        While ($TRUE)
+        {
+            $ObjShell.SendKeys("{$SendKey}")
+            Write-Host "[$(Get-Date)] $SendKey'd. Waiting $IntervalSeconds seconds." -ForegroundColor Cyan
+            Start-Sleep -Seconds $IntervalSeconds
+        }
+    }
+}
