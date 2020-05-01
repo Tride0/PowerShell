e90476ce0b6559ba07c81e8ea0bb4e9b719218d7
Kyle <34794736+Tride0@users.noreply.github.com>
2020-05-01 07:36:18 -0700

    Add files via upload

---
+++ AD/Monitor/Get-DuplicateSPNs.psm1
@@ -0,0 +1,18 @@
+﻿Function Get-DuplicateSPNs
+{
+    <#
+        .NOTES
+            Created By: Kyle Hewitt
+            Created On: 4/30/20
+
+        .DESCRIPTION
+            Get Duplicate SPNs in a domain
+    #>
+    Param(
+        $Domain = $env:USERDNSDOMAIN
+    )
+    Process
+    {
+        SetSPN.exe -t $Domain -x -p
+    }
+}
