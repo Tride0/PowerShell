50d48e19b09df94d7703d765ab6dca699e483627
Kyle <34794736+Tride0@users.noreply.github.com>
2020-05-01 07:39:25 -0700

    Create Count-NoClientSite.ps1

---
+++ AD/Sites & Services/Count-NoClientSite.ps1
@@ -0,0 +1,20 @@
+[String[]]$DCs = Get-ADDomainController -Filter * | Select-Object -ExpandProperty name 
+
+$Results = @()
+
+Foreach ($DC in $DCs)
+{
+    Remove-Variable ncs, result -ErrorAction SilentlyContinue
+
+    $NCS = Get-Content \\$DC\c$\Windows\debug\netlogon.log |
+        Where-Object -FilterScript { $_ -like '*NO_CLIENT_SITE*' }
+
+    [PSCustomObject]@{
+        DC = $DC
+        NCSCount = $NCS.Count
+    } | Tee-Object -Variable result
+    $Results += $Result
+}
+
+Clear
+$Results | Sort-Object -Property NCSCount -Descending
