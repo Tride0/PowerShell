ea8893826fa82420a49b2d1480603990bfc9c444
Kyle <34794736+Tride0@users.noreply.github.com>
2020-05-01 09:59:36 -0700

    Add files via upload

---
+++ AD/Monitor/Get-StaleADGroups.ps1
@@ -0,0 +1,25 @@
+﻿<#
+    Created By: Kyle Hewitt
+    Created On: 10/03/2018
+#>
+
+$ExportPath = "$PSScriptRoot\AD_Group_Usage.csv"
+
+$90Days = [DateTime]::Now.AddDays(-90)
+
+$ADFilter = { (WhenCreated -lt $90Days) -and (WhenChanged -lt $90Days) }
+
+$WhereFilter = { $_.Members.Count -eq 0 -and $_.Member.Count -eq 0 -and $_.Memberof.Count -eq 0 }
+
+$ADProperties = 'members', 'member', 'memberof', 'WhenCreated', 'WhenChanged', 'mail'
+
+$SelectProperties = 'Name', 'DistinguishedName', 'WhenCreated', 'WhenChanged', 'mail'
+
+$SortProperty = 'WhenChanged'
+
+Get-ADGroup -Properties $ADProperties -Filter $ADFilter |
+    Where-Object -FilterScript $WhereFilter |
+    Select-Object -Property $SelectProperties | 
+    Sort-Object -Property $SortProperty |
+    Export-csv -Path $ExportPath -NoTypeInformation -Force
+
