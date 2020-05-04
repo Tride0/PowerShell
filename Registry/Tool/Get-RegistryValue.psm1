95ecb1714a8699cef461cf2a22807eeff75e0e82
Kyle <34794736+Tride0@users.noreply.github.com>
2020-05-04 11:23:59 -0700

    Create Get-RegistryValue.psm1
    This tool has a builtin in registry navigation if a full path isn't provided to it.

---
+++ Registry/Tool/Get-RegistryValue.psm1
@@ -0,0 +1,171 @@
+Function Get-RegistryValue
+{
+    Param(
+        $Computer = $env:COMPUTERNAME,
+        $BaseKey = 'LocalMachine',
+        $SubKey,
+        $ValueName
+    )
+    
+    Function Select-Choice
+    {
+        Param(
+            [String[]]$Options,
+            $PromptPrefix,
+            $PromptSuffix,
+            $OtherOptions
+        )
+        $Prompt = $PromptPrefix
+        If ($Options.Count -ge 1)
+        {
+            $Prompt += for ($i = 0; $i -lt $Options.Count; $i++) { "`n[$($i+1)] $($Options[$i])" }
+        }
+        Else
+        {
+            $Prompt += "`n`nNO OPTIONS AVAILABLE`n"
+        }
+        $Prompt += "$PromptSuffix"
+        $Prompt += "`nTo search: Type anything aside from a legit option"
+        If ([Boolean]$Script:AllOptions) { $Prompt += "`nTo show All Options type `"All Options`"" }
+        $Prompt += "`nSelect Choice"
+
+        Switch (Read-host -Prompt $Prompt)
+        {
+            { $OtherOptions -contains $_ } { 
+                Remove-Variable -Name AllOptions -Scope Script -ErrorAction SilentlyContinue
+                Return $_ 
+            }
+
+            { ((1..$($Options.Count)) -contains $_ ) } { Return [int]$_ }
+
+            Default { 
+                If ($_ -eq "All Options")
+                {
+                    $Options = $Script:AllOptions
+                    Remove-Variable -Name AllOptions -Scope Script -ErrorAction SilentlyContinue
+                }
+                Else
+                {
+                    [String[]]$PossibleOptions = $Options -like "*$_*"
+                    If ($PossibleOptions.Count -eq $Options.Count -or $PossibleOptions.Count -eq 0)
+                    {
+                        Write-Warning "Not a validate option. Options could not be narrowed down."
+                    }
+                    ElseIf ($PossibleOptions.Count -eq 1)
+                    {
+                        Remove-Variable -Name AllOptions -Scope Script -ErrorAction SilentlyContinue
+                        Return $Options.IndexOf("$PossibleOptions")+1
+                    }
+                    ElseIf ($PossibleOptions.Count -gt 1)
+                    {
+                        $Script:AllOptions = $Options
+                        $Options = $PossibleOptions 
+                    }
+                    
+                }
+                Select-Choice -Options $Options -PromptPrefix $PromptPrefix -OtherOptions $OtherOptions
+            }
+        }
+    }
+
+    Function Navigate
+    {
+        Param(
+            $CurrentKeyLocation
+        )
+        :SubKey While ($Choice -ne 'Stay')
+        {
+            $Split = $NewLocation = $PromptPrefix = $SubKeys = $Choice = $null
+
+            # Prompt for Choice
+            $SubKeys = $CurrentKeyLocation.GetSubKeyNames() | Sort-Object
+            $PromptPrefix = "`n`nCurrent Location: $($CurrentKeyLocation.Name)"
+            $PromptPrefix += "`n[..] Previous Location"
+            $PromptPrefix += "`n[Stay] Stay at current location"
+            $Choice = Select-Choice -Options $SubKeys -PromptPrefix $PromptPrefix -OtherOptions ('..','Stay')
+            
+            If ($Choice -is [int])
+            {
+                $CurrentKeyLocation = $CurrentKeyLocation.OpenSubKey($SubKeys[($Choice-1)])
+            }
+
+            # To previous location
+            If ($Choice -eq '..')
+            {
+                $Split = $CurrentKeyLocation.Name.Split('\')
+                If ($Split.COunt -gt 2)
+                {
+                    $NewLocation = $Split[1..($Split.Count-2)] -join '\\'
+                    $CurrentKeyLocation = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($BaseKey, $Computer).OpenSubKey($NewLocation)
+                }
+                ElseIf ($Split.Count -eq 2)
+                {
+                    $CurrentKeyLocation = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($BaseKey, $Computer)
+                }
+                ElseIf ($Split.Count -eq 1)
+                {
+                    $RegBaseKey = Get-RegBaseKey
+                    $CurrentKeyLocation = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($RegBaseKey, $Computer)
+                }
+            }
+        }
+        Return $CurrentKeyLocation
+    }
+
+    Function Get-RegValue
+    {
+        Param(
+            $CurrentKeyLocation
+        )
+        # Prompt for Choice
+        $ValueNames = $CurrentKeyLocation.GetValueNames() | Sort-Object
+
+        $PromptPrefix = "`n`n`nCurrent Location: $($CurrentKeyLocation.Name)"
+        $PromptPrefix += "`nSelect an option to get it's value"
+        $PromptPrefix += "`n[..] Go back to navigation"
+        $PromptSuffix = "`n[All] Get all values"
+        $Choice = Select-Choice -Options $ValueNames -PromptPrefix $PromptPrefix -PromptSuffix $PromptSuffix -OtherOptions '..', 'All'
+        
+        Switch ($Choice)
+        {
+            '..' { Get-RegValue (Navigate -CurrentKeyLocation $CurrentKeyLocation) }
+            'All' { 
+                $AllValues = @{}
+                Foreach ($ValueName in $ValueNames)
+                {
+                    $AllValues.Add( $ValueName, $CurrentKeyLocation.GetValue($ValueName) )
+                }
+                Return [PSCustomObject]$AllValues
+            }
+            Default { Return $CurrentKeyLocation.GetValue($ValueNames[($Choice-1)]) }
+        }
+    }
+
+    Function Get-RegBaseKey
+    {
+        # Prompt for Choice
+        $BaseKeys = ([Microsoft.Win32.Registry].GetFields().Name) | Sort-Object
+        $PromptPrefix += "`nSelect an option to go to that Base Key"
+        $Choice = Select-Choice -Options $BaseKeys -PromptPrefix $PromptPrefix
+        Return $BaseKeys[($Choice-1)]
+        
+    }
+    
+    If (![Boolean]$SubKey)
+    {
+        $CurrentKeyLocation = Navigate -CurrentKeyLocation ([Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($BaseKey, $Computer))
+    }
+    Else
+    {
+        $CurrentKeyLocation = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($BaseKey, $Computer).OpenSubKey($SubKey)
+    }
+
+    If (![Boolean]$ValueName)
+    {
+        Get-RegValue -CurrentKeyLocation $CurrentKeyLocation
+    }
+    Else
+    {
+        $CurrentKeyLocation.GetValue($ValueName)
+    }
+}
